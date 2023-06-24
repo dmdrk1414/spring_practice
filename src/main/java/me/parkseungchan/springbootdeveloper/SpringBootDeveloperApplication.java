26748bb071d3ce0c78ef2944f3cfec966f39c1dd
dmdrk1414 <seungchan141414@gmail.com>
2023-06-24 23:09:11 +0900

    [Add] Spring boot 의 main 메서드를 나타낸다.

--- src/main/java/me/parkseungchan/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/parkseungchan/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -3,8 +3,8 @@ package me.parkseungchan.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootDeveloperApplication {
+@SpringBootApplication //
+public class SpringBootDeveloperApplication { // spring boot 의 main 메서드를 의미한다.
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
