d8083964dd6765f745075fd2b05d3ba1e94ae358
dmdrk1414 <seungchan141414@gmail.com>
2023-06-25 16:01:52 +0900

    [Add] sql 문과 application.yml 문 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-INSERT INTO member (id, name) VALUES (1, 'name 1')
-INSERT INTO member (id, name) VALUES (2, 'name 2')
-INSERT INTO member (id, name) VALUES (3, 'name 3')
+INSERT INTO member (id, name)
+VALUES (1, '이름1');
+INSERT INTO member (id, name)
+VALUES (2, '이름2');
+INSERT INTO member (id, name)
+VALUES (3, '이름3');
