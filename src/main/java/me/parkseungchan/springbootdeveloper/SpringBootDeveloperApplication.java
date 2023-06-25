8ddac094ac89fbd3d4252e6531727e4463d4c583
dmdrk1414 <seungchan141414@gmail.com>
2023-06-25 11:20:21 +0900

    [Add] SpringBootApplication 은 main 부분

--- src/main/java/me/parkseungchan/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/parkseungchan/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -3,7 +3,7 @@ package me.parkseungchan.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// 프레젠테이션 계층 - 컨트롤러
+
 @SpringBootApplication
 public class SpringBootDeveloperApplication { // spring boot 의 main 메서드를 의미한다.
     public static void main(String[] args) {
