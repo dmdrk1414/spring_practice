2098480dc6565d33e565f2afacd342ccd20b7849
dmdrk1414 <seungchan141414@gmail.com>
2023-06-26 18:02:36 +0900

    [Test] Test 실패한 테스트 만들기

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-// 테스트 코드 만드는 법
+// 테스트 코드 만드는 트
 public class JUnitTest {
     @DisplayName("1 + 2는 3이다.")
     @Test
@@ -13,5 +13,16 @@ public class JUnitTest {
 
         // assertEquals  첫 번째 인수 : 기대하는 값, 두 번째 인수 : 실제로 검증할 값
         Assertions.assertEquals(a + b, sum);
+    }
+
+    @DisplayName(("1 + 3는 4이다. 실패한 테스트"))
+    @Test
+    public void junitFailedTest() {
+        int a = 1;
+        int b = 3;
+        int sum = 3;
+
+        // 실패한 결과
+        Assertions.assertEquals(a + b, sum); // 실패한 케이스
     }
 }
